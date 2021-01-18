f3f81bfc69e1c74f3037d8ce6c5869c8b569d227
Gabriel Miranda <63580608+Gabrie2121@users.noreply.github.com>
2021-01-18 14:20:34 -0300

    v3

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Make-Chat
+
+### v1 
+Layout da tela de Registro finalizado, com digitalização do nome de usuario para entrada no chat
+### v2
+Iniciando UI da tela de chat
+### v3
+Tela de Registro finalizada, enviando para a SessionStorage o nome de Usuario para uso futuro
---
+++ script/ativacaoRegitro.js
@@ -0,0 +1,12 @@
+const input = document.querySelector('#user-name')
+const btn = document.querySelector('#Join')
+
+btn.addEventListener('click', () => {
+    if(!input.value == false){
+        window.location.href = './chat.html'
+        sessionStorage.setItem('username',input.value)
+    }else{
+        input.style = 'border:2px solid red'
+        alert('Você precisa Digitar um nome')
+    }
+})
